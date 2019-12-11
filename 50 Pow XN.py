f876b3ab50e35ee2610bd031790ba9baeb519304
kang_vincent_zhou <kangzhou.vicent@gmail.com>
2019-12-11 13:29:44 -0500

    Create 50 Pow XN.py

---
+++ 50 Pow XN.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n==0:
+            return 1.0
+        
+        elif n <0:
+            return 1/self.myPow(x, -n)
+        elif n%2:
+            return self.myPow(x*x, n//2)*x
+        
+        elif n%2==0:
+            return self.myPow(x*x, n//2)
