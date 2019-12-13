38ce29132aa8a76b71ec352d35741722608a03e9
kang_vincent_zhou <kangzhou.vicent@gmail.com>
2019-12-13 15:34:09 -0500

    Update 11_Container With Most Water.py

--- 11_Container With Most Water.py
+++ 11_Container With Most Water.py
@@ -15,3 +15,34 @@ class Solution(object):
             else:
                 left+=1
         return ans
+
+    
+    
+    
+    class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right=0, len(height)-1
+        res=0
+        while left<right:
+            water=min(height[right], height[left])*(right-left)
+            if res<water:
+                res=water
+                
+            if height[left]>height[right]:
+                cur=height[right]
+                right-=1
+                while left<right and height[right]<cur:
+                    right-=1
+                    
+            else:
+                cur=height[left]
+                left+=1
+                while left<right and height[left]<cur:
+                    left+=1
+                    
+        return res
+        
